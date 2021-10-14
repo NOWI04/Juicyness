99d1452381aafb52a606355a15149646cc4ecf82
emmaomfors <emma.omfors@elev.ga.lbs.se>
2021-10-14 13:13:56 +0200

    fiende

--- Assets/Scrips/EnemySpawn.cs
+++ Assets/Scrips/EnemySpawn.cs
@@ -4,44 +4,22 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
+	public GameObject Object;
+	float timebtwspawn;
+	public float startTimeBtwSpawn;
+//	public float spawnDelay;
 
-  public float spawnTime;        // The amount of time between each spawn.
-     public float spawnDelay;       // The amount of time before spawning starts.
-     public GameObject enemy;
-
-     Transform target;
-     Transform myTransform;
- 
-     void Awake()
-     {
-         myTransform = transform;
- 
-     }
- 
-     void Start()
-     {
-         GameObject stop = GameObject.FindGameObjectWithTag("Player");
- 
-         target = stop.transform;
- 
- 
-         StartCoroutine(SpawnTimeDelay());
-     }
- 
-     IEnumerator SpawnTimeDelay()
-     {
-         while (true)
-             {
-                 if (Vector3.Distance(target.position, myTransform.position) < 0)
-                 {
-                 Instantiate(enemy, transform.position, Quaternion.identity);
-                 yield return new WaitForSeconds(spawnTime);
-                 }
- 
-                 if (Vector3.Distance(target.position, myTransform.position) > 0)
-                 {
-                     yield return null;
-                 }                
-             }
-     }
- }
+	void Start()
+	{
+		if (timebtwspawn <= 0)
+		{
+			Instantiate(Object, transform.position, Quaternion.identity);
+			
+		}
+		else
+		{
+			timebtwspawn -= Time.deltaTime;
+		}
+		
+	}
+}

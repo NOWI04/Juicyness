6a80e3027c6d5c8ab14032e51351f75ce25edf17
MarWoxell <79454734+MarWoxell@users.noreply.github.com>
2021-10-21 09:28:49 +0200

    Score typ klar

--- Assets/Scrips/ScoreManager.cs
+++ Assets/Scrips/ScoreManager.cs
@@ -6,49 +6,81 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Bra cod som ska sparas
+    // Skrivet av Markus
 
-
+    // Poäng värden
     public int score = 0;
+    public int bestScore = 0;
+
+    // ska bort
     public Rigidbody rb;
-    public Text text;
+
+    // För spelscenen
+    public Text currentScore;
+    public Text highScore;
+
+
+    // För death screen
+    public Text finalScore;
+    public Text bestFinalScore;
+
 
 
 
     public void Start()
     {
-
+        score = 0;
+        bestScore = PlayerPrefs.GetInt("Score", 0);
+        highScore.text = "Highscore " + bestScore;
     }
 
     public void Update()
     {
-        // Behöver ändras för att reagera när fiender dör
 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SaveScore();
+            print("Kollat");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            bestScore = PlayerPrefs.GetInt("Score", 0);
+            print("Ladningfunkion");
+            finalScore.text = "Score " + score;
+            bestFinalScore.text = "HighScore " + bestScore;
+        }
+
+        // Behöver ändras för att reagera när fiender dör
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.transform.position = rb.transform.position + new Vector3(0, 1, 0);
             score += 1;
-            text.text = "" + score;
+            currentScore.text = "Score " + score;
         }
     }
 
     // Ska hända när spelaren dör
 
     public void SaveScore()
-    {
-
-        PlayerPrefs.SetInt("Score", score);
-        print("Sparning");
+    {   
+            if (score > bestScore)
+        {
+            bestScore = score;
+            PlayerPrefs.SetInt("Score", bestScore);
+            currentScore.text = "Score " + score;
+            print("Nytt Highscore");
+        }
+        
+       
     }
 
 
     //Ska hända i death screenen 
     public void LoadScore()
     {
-        score = PlayerPrefs.GetInt("Score", 0);
-        print("Ladningfunkion");
-        text.text = "" + score;
+      
     }
 
 
